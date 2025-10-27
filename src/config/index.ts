7877c944ba3863d1d2e7394fef019fa21023fb94
Josueprogramer <josueabdiasazafabian@gmail.com>
2025-10-26 23:41:45 -0500

    Fix: variables .env corregidas

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 
 export const config = {
   PORT: process.env.PORT ?? 8080,
-  //META
-  jwtToken: process.env.jwtToken,
-  numberId: process.env.numberId,
-  verifyToken: process.env.verifyToken,
-  version: "v22.0" // 👉 si no pones en .env, usa v20.0
+  // META
+  jwtToken: process.env.JWT_TOKEN,
+  numberId: process.env.NUMBER_ID,
+  verifyToken: process.env.VERIFY_TOKEN,
+  version: "v22.0" // o la versión que uses
 };
